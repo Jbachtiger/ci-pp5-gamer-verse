571435aa394eb72d7b43d48e653c281dda82e0af
Jbachtiger <jbachtiger@gmail.com>
2022-10-06 15:03:41 +0000

    Add basic styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="60px" />
@@ -12,12 +13,12 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link><i class="fas fa-users"></i> Community Hub</Nav.Link>
-            <Nav.Link><i class="fas fa-star"></i> Reviews</Nav.Link>
-            <Nav.Link><i class="fas fa-calendar-check"></i> Events</Nav.Link>
-            <Nav.Link><i class="fas fa-sign-in-alt"></i> Login</Nav.Link>
-            <Nav.Link><i class="fas fa-user-plus"></i> Sign Up</Nav.Link>
-            <Nav.Link><i class="fas fa-address-card"></i> About</Nav.Link>
+            <Nav.Link><i className="fas fa-users"></i> Community Hub</Nav.Link>
+            <Nav.Link><i className="fas fa-star"></i> Reviews</Nav.Link>
+            <Nav.Link><i className="fas fa-calendar-check"></i> Events</Nav.Link>
+            <Nav.Link><i className="fas fa-sign-in-alt"></i> Login</Nav.Link>
+            <Nav.Link><i className="fas fa-user-plus"></i> Sign Up</Nav.Link>
+            <Nav.Link><i className="fas fa-address-card"></i> About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
