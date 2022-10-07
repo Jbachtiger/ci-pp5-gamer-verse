96fe11a5122e25a83e0fc7ded051183a1857eebd
Jbachtiger <jbachtiger@gmail.com>
2022-10-07 16:24:56 +0000

    Add signin component template and link to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import {Route, Switch} from "react-router-dom"
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route exact path="/" render={() => <h1>Community Hub</h1>}/>
         <Route exact path="/reviews" render={() => <h1>Reviews</h1>}/>
         <Route exact path="/events" render={() => <h1>Events</h1>}/>
-        <Route exact path="/signin" render={() => <h1>Sign In</h1>}/>
+        <Route exact path="/signin" render={() => <SignInForm />}/>
         <Route exact path="/signup" render={() => <SignUpForm />}/>
         <Route exact path="/about" render={() => <h1>About</h1>}/>
         <Route render={() => <h1>Page not found!</h1>}/>
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Form, Button, Container, Alert } from "react-bootstrap";
+import styles from "../../styles/SignUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import axios from "axios";
+
+/**
+ * Renders SignUp form
+ * Moments walkthrough used as a guide for variables, data handling and error handling code
+ */
+const SignInForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  /**
+   * Converts inputed data into Key:Value pairs
+   */
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+  
+  /**
+   * Pushes data to API
+   * Redirects the user to signin page
+   * Error messages displayed for invalid data inputs
+   */
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Container className={styles.Container}>
+      <h1>Sign In!</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+        {errors.username?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group controlId="password1">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            name="password1"
+            value={password1}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.password1?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group controlId="password2">
+          <Form.Label>Confirm password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Re-enter password"
+            name="password2"
+            value={password2}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.password2?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Button
+          className={`${btnStyles.Button} ${btnStyles.Wide}`}
+          type="submit"
+        >
+          Sign Up
+        </Button>
+        {errors.non_field_errors?.map((message, idx) => (
+          <Alert variant="warning" className="mt-4" key={idx}>
+            {message}
+          </Alert>
+        ))}
+      </Form>
+
+      <div>
+        <p>
+          Already have an account?
+          <Link to="/signin" className={styles.Link}>
+            <strong> Sign in here!</strong>
+          </Link>
+        </p>
+      </div>
+    </Container>
+  );
+};
+
+export default SignInForm;
