b2381546e51f7f79a6e071458ee84c2ee8734c93
Jbachtiger <jbachtiger@gmail.com>
2022-10-07 12:23:00 +0000

    Add style to navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,19 +9,31 @@ const NavBar = () => {
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="60px" />
-        </Navbar.Brand>
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="60px" />
+          </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink to="/"><i className="fas fa-users"></i> Community Hub</NavLink>
-            <NavLink to="/reviews"><i className="fas fa-star"></i> Reviews</NavLink>
-            <NavLink to="/events"><i className="fas fa-calendar-check"></i> Events</NavLink>
-            <NavLink to="/signin"><i className="fas fa-sign-in-alt"></i> Sign In</NavLink>
-            <NavLink to="/signup"><i className="fas fa-user-plus"></i> Sign Up</NavLink>
-            <NavLink to="/about"><i className="fas fa-address-card"></i> About</NavLink>
+            <NavLink className={styles.NavLink} to="/">
+              <i className="fas fa-users"></i> Community Hub
+            </NavLink>
+            <NavLink className={styles.NavLink} to="/reviews">
+              <i className="fas fa-star"></i> Reviews
+            </NavLink>
+            <NavLink className={styles.NavLink} to="/events">
+              <i className="fas fa-calendar-check"></i> Events
+            </NavLink>
+            <NavLink className={styles.NavLink} to="/signin">
+              <i className="fas fa-sign-in-alt"></i> Sign In
+            </NavLink>
+            <NavLink className={styles.NavLink} to="/signup">
+              <i className="fas fa-user-plus"></i> Sign Up
+            </NavLink>
+            <NavLink className={styles.NavLink} to="/about">
+              <i className="fas fa-address-card"></i> About
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
