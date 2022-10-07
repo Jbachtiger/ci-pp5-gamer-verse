dd8c2a625c8d68877a990a88b676354b32360992
Jbachtiger <jbachtiger@gmail.com>
2022-10-07 14:13:54 +0000

    Add styling to signup form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,35 +1,56 @@
 import React from "react";
-import {Form, Button} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Form, Button, Container } from "react-bootstrap";
+import styles from "../../styles/SignUpForm.module.css";
 
 const SignUpForm = () => {
   return (
-    <div>
-      <h1>SignUp</h1>
-
+    <Container className={styles.Container}>
+      <h1>Sign Up!</h1>
       <Form>
         <Form.Group controlId="username">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" name="username" />
-          <Form.Text className="text-muted">
-          </Form.Text>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            name="username"
+          />
+          <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
 
         <Form.Group controlId="password1">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" name="password" />
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            name="password"
+          />
         </Form.Group>
 
         <Form.Group controlId="password2">
           <Form.Label>Confirm password</Form.Label>
-          <Form.Control type="password" placeholder="Re-enter password" name="password2" />
+          <Form.Control
+            type="password"
+            placeholder="Re-enter password"
+            name="password2"
+          />
         </Form.Group>
 
-        
         <Button variant="primary" type="submit">
           Sign Up
         </Button>
       </Form>
-    </div>
+
+      <div>
+        <p>Already have an account?
+        <Link to="/signin" className={styles.Link}>
+        <strong> Sign in here!</strong>
+        </Link>
+        </p>
+      </div>
+
+
+    </Container>
   );
 };
 
