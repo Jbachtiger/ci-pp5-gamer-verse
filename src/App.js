06ae89d0e1f34dfb37e4e085d792efacd7497f3e
Jbachtiger <jbachtiger@gmail.com>
2022-10-07 12:56:57 +0000

    Add signup component and stylesheet. Link to App.js file.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import { Container } from "react-bootstrap";
 import {Route, Switch} from "react-router-dom"
 import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route exact path="/reviews" render={() => <h1>Reviews</h1>}/>
         <Route exact path="/events" render={() => <h1>Events</h1>}/>
         <Route exact path="/signin" render={() => <h1>Sign In</h1>}/>
-        <Route exact path="/signup" render={() => <h1>Sign Up</h1>}/>
+        <Route exact path="/signup" render={() => <SignUpForm />}/>
         <Route exact path="/about" render={() => <h1>About</h1>}/>
         <Route render={() => <h1>Page not found!</h1>}/>
       </Switch>
