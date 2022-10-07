15a5979d89ca7316d9cb87175f6a450b076c00b5
Jbachtiger <jbachtiger@gmail.com>
2022-10-07 11:08:04 +0000

    Add basic routing functionality and test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import styles from "./App.module.css"
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import {Route, Switch} from "react-router-dom"
 
 
 function App() {
   return (
   <div className={styles.App}>
     <NavBar/>
+    <Container className={styles.Main}>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Community Hub</h1>}/>
+        <Route exact path="/reviews" render={() => <h1>Reviews</h1>}/>
+        <Route exact path="/events" render={() => <h1>Events</h1>}/>
+        <Route exact path="/signin" render={() => <h1>Sign In</h1>}/>
+        <Route exact path="/signup" render={() => <h1>Sign Up</h1>}/>
+        <Route exact path="/about" render={() => <h1>About</h1>}/>
+      </Switch>
+
+    </Container>
   </div>  
 
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,26 @@ import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+        <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" height="60px" />
         </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link><i className="fas fa-users"></i> Community Hub</Nav.Link>
-            <Nav.Link><i className="fas fa-star"></i> Reviews</Nav.Link>
-            <Nav.Link><i className="fas fa-calendar-check"></i> Events</Nav.Link>
-            <Nav.Link><i className="fas fa-sign-in-alt"></i> Login</Nav.Link>
-            <Nav.Link><i className="fas fa-user-plus"></i> Sign Up</Nav.Link>
-            <Nav.Link><i className="fas fa-address-card"></i> About</Nav.Link>
+            <NavLink to="/"><i className="fas fa-users"></i> Community Hub</NavLink>
+            <NavLink to="/reviews"><i className="fas fa-star"></i> Reviews</NavLink>
+            <NavLink to="/events"><i className="fas fa-calendar-check"></i> Events</NavLink>
+            <NavLink to="/signin"><i className="fas fa-sign-in-alt"></i> Sign In</NavLink>
+            <NavLink to="/signup"><i className="fas fa-user-plus"></i> Sign Up</NavLink>
+            <NavLink to="/about"><i className="fas fa-address-card"></i> About</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
