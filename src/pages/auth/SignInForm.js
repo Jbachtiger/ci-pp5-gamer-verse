c365ae3a1e97795545d48a5a458ca8aca1a77864
Jbachtiger <jbachtiger@gmail.com>
2022-10-07 16:35:19 +0000

    Add styling to signin form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Form, Button, Container, Alert } from "react-bootstrap";
-import styles from "../../styles/SignUpForm.module.css";
+import styles from "../../styles/SignInForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import axios from "axios";
 
