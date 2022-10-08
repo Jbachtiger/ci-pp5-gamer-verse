f0d2a110e746143415a0f60e22d8dcf3f47b21e6
Jbachtiger <jbachtiger@gmail.com>
2022-10-08 20:30:57 +0000

    Add navbar burger toggle hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,13 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -25,7 +28,7 @@ const NavBar = () => {
 
   const loggedInIcons = (
     <>
-    <NavLink
+      <NavLink
         to="/feed"
         className={styles.NavLink}
         activeClassName={styles.Active}
@@ -70,14 +73,23 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="60px" />
           </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
