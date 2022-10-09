7183e49e157fe76b53c37a880ad9f30e1d9eb9c3
Jbachtiger <jbachtiger@gmail.com>
2022-10-09 12:21:12 +0000

    Add PostCreateForm function boilerplate and import into App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Switch} from "react-router-dom"
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route exact path="/events" render={() => <h1>Events</h1>}/>
         <Route exact path="/signin" render={() => <SignInForm />}/>
         <Route exact path="/signup" render={() => <SignUpForm />}/>
+        <Route exact path="/posts/create" render={() => <PostCreateForm />}/>
         <Route exact path="/about" render={() => <h1>About</h1>}/>
         <Route render={() => <h1>Page not found!</h1>}/>
       </Switch>
---
+++ src/pages/posts/PostCreateForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostCreateForm = () => {
+  return (
+   <h1>PostCreateForm </h1>
+  )
+}
+
+export default PostCreateForm
