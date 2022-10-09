265aa944b2fbb6e7697c8c420df257cdf34d34a4
Jbachtiger <jbachtiger@gmail.com>
2022-10-09 13:24:53 +0000

    Add handleChange function for form data handling

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
+/**
+ * Render PostCreate form
+ * Provide user with input fields to create a post
+ */
 const PostCreateForm = () => {
+    const [errors, setErrors] = useState({});
+
+    const [postData, setPostData] = useState({
+        title: "",
+        decription: "",
+        game_medium: "",
+        image: "",
+    });
+
+    const { title, description, game_medium, image } = postData
+
+    const imageInput = useRef(null);
+    const history = useHistory();
+
+    /**
+     * Populate the postData strings
+     */
+    const handleChange = (event) => {
+        setPostData({
+            ...postData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
   return (
     <Container>
       <Form>
