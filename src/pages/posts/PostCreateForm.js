416537d4b985da9469671bfdc205d8ab0a7147b7
Jbachtiger <jbachtiger@gmail.com>
2022-10-09 12:53:10 +0000

    Add template form fields for PostCreateForm.js

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
 
 const PostCreateForm = () => {
   return (
-   <h1>PostCreateForm </h1>
-  )
-}
+    <Container>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-export default PostCreateForm
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>What's on your mind?</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Topic you want to talk about</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>IMAGE</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+          Post
+        </Button>
+        <Button variant="primary" type="submit">
+          Cancel
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default PostCreateForm;
