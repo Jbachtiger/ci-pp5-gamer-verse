74dad58b1d972a0f740527a3ec50cdcc4d752bd5
Jbachtiger <jbachtiger@gmail.com>
2022-10-10 15:16:03 +0000

    Add function to fetch post data

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { axiosReq } from '../../api/axiosDefaults'
 import appStyles from "../../App.module.css"
 import postPageStyle from "../../styles/PostPage.module.css"
 
 
 
 const PostPage = () => {
+    const {id} =useParams();
+    const [post, setPost] = useState({results: []});
+
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const [{ data: post }] = await Promise.all([
+              axiosReq.get(`/posts/${id}`),
+            ]);
+            setPost({ results: [post] });
+            console.log(post);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        handleMount();
+      }, [id]);
+
+
     return (
         <Row className="h-100">
           <Col className="py-2 p-0 p-lg-2" lg={8}>
