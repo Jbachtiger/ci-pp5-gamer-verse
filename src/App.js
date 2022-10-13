8237b736d5db2c07d3a104e9a7b364d09a5c4e23
Jbachtiger <jbachtiger@gmail.com>
2022-10-13 10:51:40 +0000

    Add Routes for posts page, favourites and feed. Add filter to feed and favourites route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Container } from "react-bootstrap";
-import {Route, Switch} from "react-router-dom"
+import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
-
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
-  <div className={styles.App}>
-    <NavBar/>
-    <Container className={styles.Main}>
-      <Switch>
-        <Route exact path="/" render={() => <h1>Community Hub</h1>}/>
-        <Route exact path="/reviews" render={() => <h1>Reviews</h1>}/>
-        <Route exact path="/events" render={() => <h1>Events</h1>}/>
-        <Route exact path="/signin" render={() => <SignInForm />}/>
-        <Route exact path="/signup" render={() => <SignUpForm />}/>
-        <Route exact path="/posts/create" render={() => <PostCreateForm />}/>
-        <Route exact path="/posts/:id" render={() => <PostPage/>}/>
-        <Route exact path="/about" render={() => <h1>About</h1>}/>
-        <Route render={() => <h1>Page not found!</h1>}/>
-      </Switch>
-
-    </Container>
-  </div>  
-
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <PostsPage message="No results found." />}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results found."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/favourites"
+            render={() => (
+              <PostsPage
+                message="No results found."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_on&`}
+              />
+            )}
+          />
+          <Route exact path="/reviews" render={() => <h1>Reviews</h1>} />
+          <Route exact path="/events" render={() => <h1>Events</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/about" render={() => <h1>About</h1>} />
+          <Route render={() => <h1>Page not found!</h1>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
