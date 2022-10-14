90033608ed7cdd3a6108733ac875f0d7ac3f1abb
Jbachtiger <jbachtiger@gmail.com>
2022-10-14 16:59:23 +0000

    Create CommentCreateForm and style

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,48 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
-import { axiosReq } from '../../api/axiosDefaults'
-import appStyles from "../../App.module.css"
-import postPageStyle from "../../styles/PostPage.module.css"
-import Post from './Post'
-
-
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import postPageStyle from "../../styles/PostPage.module.css";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import Post from "./Post";
 
 const PostPage = () => {
-    const {id} =useParams();
-    const [post, setPost] = useState({results: []});
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-          try {
-            const [{ data: post }] = await Promise.all([
-              axiosReq.get(`/posts/${id}`),
-            ]);
-            setPost({ results: [post] });
-            console.log(post);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-    
-        handleMount();
-      }, [id]);
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    return (
-        <Row className="h-100">
-          <Col className="py-2 p-0 p-lg-2" lg={8}>
-            <p className={postPageStyle.TextColour}>Popular profiles for mobile</p>
-            <Post {...post.results[0]} setPosts={setPost} postPage />
-            <Container className={appStyles.Content}>
-              Comments
-            </Container>
-          </Col>
-          <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-            Popular profiles for desktop
-          </Col>
-        </Row>
-      );
-    }
+    handleMount();
+  }, [id]);
 
-export default PostPage
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p className={postPageStyle.TextColour}>Popular profiles for mobile</p>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
+    </Row>
+  );
+};
+
+export default PostPage;
