09429bdcbbcee572e379e43553ef4e02690bdf70
Jbachtiger <jbachtiger@gmail.com>
2022-10-17 14:03:34 +0000

    Add profile image, name and follow/unfollow buttons to Profile.js

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,10 +1,56 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from '../../styles/Profile.module.css'
+import btnStyles from '../../styles/Button.module.css'
+import Avatar from "../../components/Avatar";
 
-const Profile = () => {
+/**
+ * Displays profile avatar and follow/unfollow buttons
+ * Code used from Moments walthrough and amended
+ */
+const Profile = (props) => {
+  const { profile, mobile, imageSize = 50 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  
   return (
-    <div>Profiles</div>
-  )
-}
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              Unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              Follow
+            </Button>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Profile
