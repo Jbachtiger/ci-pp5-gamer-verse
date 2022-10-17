bff96abf6f394a893698251894149399cc845733
Jbachtiger <jbachtiger@gmail.com>
2022-10-17 10:55:29 +0000

    Add functionality to render profile lists for mobule

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -4,7 +4,12 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
-const PopularProfiles = () => {
+/**
+ * Retrieves profiles from Gamer Verse API
+ * Return most popular followed dependent on count
+ * Code for functions provided by Moments walkthrough
+ */
+const PopularProfiles = ({ mobile }) => {
   const [profileData, setProfileData] = useState({
     pageprofile: { results: [] },
     popularProfiles: { results: [] },
@@ -31,13 +36,21 @@ const PopularProfiles = () => {
   }, [currentUser]);
 
   return (
-    <Container>
+    <Container className={mobile && "d-lg-none text-center mb-3"}>
       {popularProfiles.results.length ? (
         <>
           <p>Most active community gamers:</p>
-          {popularProfiles.results.map((profile) => (
-            <p key={profile.id}>{profile.owner}</p>
-          ))}
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <p key={profile.id}>{profile.owner}</p>
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <p key={profile.id}>{profile.owner}</p>
+            ))
+          )}
         </>
       ) : (
         <Asset spinner />
