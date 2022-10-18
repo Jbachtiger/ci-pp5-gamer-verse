4b495a9b1755b49635f88e9d05c7f7d8490fee16
Jbachtiger <jbachtiger@gmail.com>
2022-10-18 19:41:46 +0000

    Populate UsernameForm component

--- src/pages/profiles/UsernameForm.js
+++ src/pages/profiles/UsernameForm.js
@@ -1,10 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
+import {
+  useHistory,
+  useParams,
+} from "react-router-dom/cjs/react-router-dom.min";
+import { axiosRes } from "../../api/axiosDefaults";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
+
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
 const UsernameForm = () => {
+  const [username, setUsername] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+  const { id } = useParams();
+
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() === id) {
+      setUsername(currentUser.username);
+    } else {
+      history.push("/");
+    }
+  }, [currentUser, history, id]);
+
+  /**
+   * Update API with edited username.
+   */
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put("/dj-rest-auth/user/", { username });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        username,
+      }));
+      history.goBack();
+    } catch (err) {
+      // console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
   return (
-    <div>
-      <h2>UsernameForm</h2>
-    </div>
+    <Row>
+      <Col className="py-2 mx-auto text-center" md={6}>
+        <Container className={appStyles.Content}>
+          <Form onSubmit={handleSubmit} className="my-2">
+            <Form.Group>
+              <Form.Label>Change username</Form.Label>
+              <Form.Control
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </Form.Group>
+            {errors?.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Button
+              className={btnStyles.Button}
+              onClick={() => history.goBack()}
+            >
+              Cancel
+            </Button>
+            <Button className={btnStyles.Button} type="submit">
+              Save
+            </Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
   );
 };
 
