03bce40766f207db1ac9beee993fc53e61322425
Jbachtiger <jbachtiger@gmail.com>
2022-10-19 15:44:59 +0000

    Add edit and delete functions to Review.js

--- src/pages/reviews/Review.js
+++ src/pages/reviews/Review.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosRes } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 /**
  * Render the data of a single review
  */
-const Review = () => {
-  return (
-    <div>Review</div>
-  )
-}
+const Review = (props) => {
+  const {
+    id,
+    owner,
+    created_on,
+    modified_on,
+    profile_id,
+    profile_image,
+    content,
+    image,
+    genre,
+    game_score,
+    game_publisher,
+    game_developer,
+    title,
+    ReviewPage,
+  } = props;
 
-export default Review
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  /**
+   * Divert user to review edit page
+   */
+  const handleEdit = () => {
+    history.push(`/reviews/${id}/edit`);
+  };
+
+  /**
+   * Delete review data from Gamer Verse API
+   */
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/reviews/${id}/`);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return <div>Review</div>;
+};
+
+export default Review;
