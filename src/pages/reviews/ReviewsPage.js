a0515dfc779466191f132e4033bc1410f18d3c36
Jbachtiger <jbachtiger@gmail.com>
2022-10-19 21:21:02 +0000

    Render reviews to the browser as infinite scroll list

--- src/pages/reviews/ReviewsPage.js
+++ src/pages/reviews/ReviewsPage.js
@@ -1,62 +1,123 @@
-import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
-import { axiosReq } from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import React, { useEffect, useState } from "react";
+import { Container, Form } from "react-bootstrap";
 
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+
+import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
 
-const ReviewsPage = () => {
-    function ReviewsPage({ message, filter = "" }) {
-        const [reviews, setReviews] = useState({ results: [] });
-        const [hasLoaded, setHasLoaded] = useState(false);
-        const { pathname } = useLocation();
-      
-        const [query, setQuery] = useState("");
-      
-        const currentUser = useCurrentUser();
-      
-        /**
-       * Displays addReview icon
-       */
-        const addReviewIcon = (
-          <NavLink
-            className={styles.NavLink}
-            activeClassName={styles.Active}
-            to="/reviews/create"
-          >
-            <i className="far fa-plus-square"></i>Add Review
-          </NavLink>
-        );
-      
-         /**
-       * Fetches reviews from Gamer Verse API
-       * Returns search results
-       */
-        useEffect(() => {
-          const fetchReviews= async () => {
-            try {
-              const { data } = await axiosReq.get(`/reviews/?${filter}search=${query}`);
-              setReviews(data);
-              setHasLoaded(true);
-            } catch (err) {
-              console.log(err);
-            }
-          };
-      
-          setHasLoaded(false);
-          const timer = setTimeout(() => {
-            fetchReviews();
-          }, 1000);
-      
-          return () => {
-            clearTimeout(timer);
-          };
-        }, [filter, query, pathname]);
-    
-  return (
-    <h1>Reviews Page</h1>
-  )
-}}
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
-export default ReviewsPage
+import NoResults from "../../assets/no-results.png";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { NavLink } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import PopularProfiles from "../profiles/PopularProfiles";
+import Review from "./Review";
+
+/**
+ * Displays all reviews
+ */
+
+function ReviewsPage({ message, filter = "" }) {
+  const [reviews, setReviews] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  const [query, setQuery] = useState("");
+
+  const currentUser = useCurrentUser();
+
+  /**
+ * Displays addReview icon
+ */
+  const addReviewIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/reviews/create"
+    >
+      <i className="far fa-plus-square"></i>Add Review
+    </NavLink>
+  );
+
+   /**
+ * Fetches reviews from Gamer Verse API
+ * Returns search results
+ */
+  useEffect(() => {
+    const fetchReviews= async () => {
+      try {
+        const { data } = await axiosReq.get(`/reviews/?${filter}search=${query}`);
+        setReviews(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchReviews();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <Container>{currentUser && addReviewIcon}</Container>
+        <PopularProfiles mobile />
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search reviews"
+          />
+        </Form>
+
+        {hasLoaded ? (
+          <>
+            {reviews.results.length ? (
+              <InfiniteScroll
+                children={reviews.results.map((review) => (
+                  <Review key={review.id} {...review} setReviews={setReviews} />
+                ))}
+                dataLength={reviews.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!reviews.next}
+                next={() => fetchMoreData(reviews, setReviews)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
+  );
+}
+
+export default ReviewsPage;
