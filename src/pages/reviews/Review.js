bce3515aee1f8722c33c5ff828f4c0812adc0c90
Jbachtiger <jbachtiger@gmail.com>
2022-10-19 19:43:25 +0000

    Add page layout for Review.js

--- src/pages/reviews/Review.js
+++ src/pages/reviews/Review.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Card, Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosRes } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Avatar from '../../components/Avatar';
+import { MoreDropdown } from '../../components/DropdownMenu';
+import styles from "../../styles/Post.module.css";
 
 /**
  * Render the data of a single review
@@ -47,7 +52,40 @@ const Review = (props) => {
     }
   };
 
-  return <div>Review</div>;
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+              
+          <div className="d-flex align-items-center">
+            <span>Created on: {created_on}</span>
+            {is_owner && ReviewPage && (
+              <MoreDropdown
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            )}
+          </div>
+        </Media>
+      </Card.Body>
+  {title && <Card.Title className="text-center">{title}</Card.Title>}
+      <Link to={`/reviews/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body> 
+         {content && <Card.Text>{content}</Card.Text>}
+        {genre && <Card.Text>Genre: {genre}</Card.Text>}
+        {game_score && <Card.Text>Rating: {game_score}</Card.Text>}
+        {game_publisher && <Card.Text>Game Publisher: {game_publisher}</Card.Text>}
+        {game_developer && <Card.Text>Game Developer: {game_developer}</Card.Text>}
+        {modified_on && <Card.Text>Last modified: {modified_on}</Card.Text>}
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Review;
