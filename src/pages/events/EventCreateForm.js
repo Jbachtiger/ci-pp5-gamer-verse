f3f6abf728b136216ab9968ff7605deafac0bb52
Jbachtiger <jbachtiger@gmail.com>
2022-10-20 19:27:03 +0000

    Add form to EventCreateForm.js and add initial styling

--- src/pages/events/EventCreateForm.js
+++ src/pages/events/EventCreateForm.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useRedirect } from "../../hooks/useRedirect";
 import { axiosReq } from "../../api/axiosDefaults";
+import { Alert, Button, Container, Form } from "react-bootstrap";
+
+import btnStyles from "../../styles/Button.module.css";
+import createFormStyles from "../../styles/PostCreateForm.module.css";
 
 const EventCreateForm = () => {
   useRedirect("loggedOut");
@@ -55,7 +59,7 @@ const EventCreateForm = () => {
 
     formData.append("title", title);
     formData.append("content", content);
-    formData.append("data", date);
+    formData.append("date", date);
     formData.append("time", time);
     formData.append("city", city);
     formData.append("address", address);
@@ -75,7 +79,145 @@ const EventCreateForm = () => {
       }
     }
   };
-  return <h1>Submit an Event:</h1>;
+  return (
+    <Container>
+      <h1 className={`${createFormStyles.MainTitle} mt-5`}>Submit an Event:</h1>
+      <Form onSubmit={handleSubmit} className={createFormStyles.Container}>
+        <Form.Group>
+          <Form.Label>Event Name:</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange}
+            aria-label="title"
+            placeholder="What is your events name?"
+          />
+        </Form.Group>
+        {errors?.title?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group>
+          <Form.Label>Content:</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={6}
+            name="content"
+            value={content}
+            onChange={handleChange}
+            aria-label="content"
+            placeholder="Tell us about your event"
+          />
+        </Form.Group>
+        {errors?.content?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group>
+          <Form.Label>Price:</Form.Label>
+          <Form.Control
+            type="text"
+            name="price"
+            value={price}
+            onChange={handleChange}
+            aria-label="price"
+            placeholder="How much is entry to the event?"
+          />
+        </Form.Group>
+        {errors?.price?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group>
+          <Form.Label>Date:</Form.Label>
+          <Form.Control
+            type="date"
+            name="date"
+            value={date}
+            onChange={handleChange}
+            aria-label="date"
+            placeholder="What date is your event on?"
+          />
+        </Form.Group>
+        {errors?.date?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group>
+          <Form.Label>Time:</Form.Label>
+          <Form.Control
+            type="time"
+            name="time"
+            value={time}
+            onChange={handleChange}
+            aria-label="time"
+            placeholder="What time is your event on?"
+          />
+        </Form.Group>
+        {errors?.time?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group>
+          <Form.Label>Address:</Form.Label>
+          <Form.Control
+            type="text"
+            name="address"
+            value={address}
+            onChange={handleChange}
+            aria-label="address"
+            placeholder="What is the event address?"
+          />
+        </Form.Group>
+        {errors?.address?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Form.Group>
+          <Form.Label>City:</Form.Label>
+          <Form.Control
+            type="text"
+            name="city"
+            value={city}
+            onChange={handleChange}
+            aria-label="city"
+            placeholder="What city is the event being held in?"
+          />
+        </Form.Group>
+        {errors?.city?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Button className={`${btnStyles.Button}`} type="submit">
+          Post
+        </Button>
+        <Button
+          onClick={() => history.goBack()}
+          className={`${btnStyles.Button}`}
+          type="submit"
+        >
+          Cancel
+        </Button>
+      </Form>
+      console.log(Form);
+      <br />
+    </Container>
+  );
 };
 
 export default EventCreateForm;
