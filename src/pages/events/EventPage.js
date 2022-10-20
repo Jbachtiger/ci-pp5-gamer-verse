003795751c32f892fc1f18ba41653c4dc9ed0cd5
Jbachtiger <jbachtiger@gmail.com>
2022-10-20 20:32:04 +0000

    Add function to retrieve event data from API. Render event component

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../../api/axiosDefaults';
+import Event from './Event';
 
-const EventPage = () => {
+/**
+ * Display single event details
+ */
+ const EventPage = () => {
+    const { id } = useParams();
+    const [event, setEvent] = useState({ results: [] });
+  
+  /**
+   * Retrieve event data from Gamer Verse API
+   */
+    useEffect(() => {
+      const handleMount = async () => {
+        try {
+          const [{ data: event }] = await Promise.all([
+            axiosReq.get(`/events/${id}`),
+          ]);
+          setEvent({ results: [event] });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+  
+      handleMount();
+    }, [id]);
+  
   return (
-    <div>EventPage</div>
-  )
-}
-
-export default EventPage
+      <Container>
+        <Col>
+          <Event {...event.results[0]} setEvent={setEvent} eventPage />
+        </Col>
+      </Container>
+    );
+  }
+  
+  export default EventPage
