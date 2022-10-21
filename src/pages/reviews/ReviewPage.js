d97d0f5c8eca754743bcb1781c529569bebc6812
Jbachtiger <jbachtiger@gmail.com>
2022-10-21 16:02:21 +0000

    Update styling to ReviewPage.js

--- src/pages/reviews/ReviewPage.js
+++ src/pages/reviews/ReviewPage.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { Col, Container } from 'react-bootstrap';
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import { axiosReq } from '../../api/axiosDefaults';
-import Review from './Review';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import Review from "./Review";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 /**
  * Display single review details
@@ -11,9 +12,9 @@ const ReviewPage = () => {
   const { id } = useParams();
   const [review, setReview] = useState({ results: [] });
 
-/**
- * Retrieve review data from Gamer Verse API
- */
+  /**
+   * Retrieve review data from Gamer Verse API
+   */
   useEffect(() => {
     const handleMount = async () => {
       try {
@@ -29,13 +30,19 @@ const ReviewPage = () => {
     handleMount();
   }, [id]);
 
-return (
+  return (
     <Container>
-      <Col>
-        <Review {...review.results[0]} setReview={setReview} reviewPage />
+      <Row className="h-100">
+        <Col className="py-2 p-0 p-lg-4" lg={8}>
+          <PopularProfiles mobile />
+          <Review {...review.results[0]} setReview={setReview} reviewPage />
+        </Col>
+        <Col lg={4} className="d-none d-lg-block p-0 p-lg-0">
+        <PopularProfiles />
       </Col>
+      </Row>
     </Container>
   );
-}
+};
 
-export default ReviewPage
+export default ReviewPage;
