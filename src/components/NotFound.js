8578f102b06f6e43d708f97d65ee9e32a7eff85d
Jbachtiger <jbachtiger@gmail.com>
2022-10-26 10:22:41 +0000

    Populate NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import NoResults from "../assets/no-results.png";
+import Asset from "./Asset";
 
 const NotFound = () => {
   return (
-    <div>Page Not Found</div>
-  )
-}
+    <Container>
+      <div>
+        <Asset
+          src={NoResults}
+          message={`Sorry, the page you are looking for was not found! Go back and try a different link!`}
+        />
+      </div>
+    </Container>
+  );
+};
 
-export default NotFound
+export default NotFound;
